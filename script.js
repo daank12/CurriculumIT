8278864274ff0628f34247beeb92a86a9a11c266
daank12 <96422551+daank12@users.noreply.github.com>
2023-02-10 13:29:52 -0300

    Fixeamos problemas y dimos funcionalidad a contact

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log('hola mundo');
+let contactButton = document.getElementById('contactButton');
+let detailsOn = document.getElementsByClassName('details-info');
+let showContent = document.getElementById('showContact');
+
+document.addEventListener('click', function () {
+        console.log('click');
+        var x = document.getElementById("details-info");
+        if (x.style.display === "none" && showContent.innerText == "Mostrar Contactos") {
+            x.style.display = "flex";
+            showContent.innerText = "Ocultar Contactos";
+        } else {
+            x.style.display = "none";
+            showContent.innerText = "Mostrar Contactos";
+        }
+    }
+);
+if (window.screen.width > '767px'){
+    detailsOn.style.display = 'flex';
+} else {
+    // detailsOn.style.display = 'none';
+
+}
