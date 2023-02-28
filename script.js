b260869a11b45640d1f270e1304293f75dcbb989
Maxi Sotelo <96422551+daank12@users.noreply.github.com>
2023-02-27 22:42:45 -0300

    aplicamos fetch data para la info

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 let contactButton = document.getElementById('contactButton');
 let detailsOn = document.getElementsByClassName('details-info');
 let showContent = document.getElementById('showContact');
+const foto = document.querySelector(".picture")
+const name = document.querySelector(".name")
+const job = document.querySelector(".job")
+const mail = document.querySelector(".mail")
 
-document.addEventListener('click', (contactButton, MouseEvent) =>{
+const tel = document.querySelector(".tel")
+const loqation =document.querySelector(".location")
+
+// MOSTRAR CONTACTOS
+showContent.addEventListener('click',() =>{
         console.log('click');
         var x = document.getElementById("details-info");
         if (x.style.display === "none" && showContent.innerText == "Mostrar Contactos") {
@@ -15,9 +23,44 @@ document.addEventListener('click', (contactButton, MouseEvent) =>{
         }
     }
 );
+// RESPONSIVE
 if (window.screen.width > '767px'){
     detailsOn.style.display = 'flex';
 } else {
     // detailsOn.style.display = 'none';
 
 }
+// API CONFIG
+document.querySelector('#example').addEventListener('click', ()=> {
+    obtenerDatos();
+})
+
+const obtenerDatos = async()=>{
+    console.log('diste log')
+    try {
+    const url = 'https://randomuser.me/api';
+    const respuesta = await fetch (url);
+    const { results } = await respuesta.json();
+    const datos = results[0]
+
+    foto.src = datos.picture.medium;
+    name.textContent = (datos.name.first + " " + datos.name.last);
+    tel.textContent = datos.phone;
+    loqation.textContent= datos.location.country;
+    mail.textContent =datos.email;
+    
+
+    
+    console.log(datos)
+    }catch (error){
+        console.log(error);
+    }
+
+
+    
+    
+
+    
+    
+
+} 
